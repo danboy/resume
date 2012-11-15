651325dfd0258e07a6884fb997b532eba876e9b0
Dan Nawara <dnawara@groupon.com>
2012-11-14 22:24:36 -0600

    starting on slide.js

--- public/javascripts/slide.js
+++ public/javascripts/slide.js
@@ -1,12 +1,57 @@
 var Movie = function(cells, options){
-  var stages = document.getElementsByClassName(cells);
+  this.stages = document.getElementsByClassName(cells);
   this.options = {
-      foo: 'hello'
-    , egg: 'muffin'
+      next: 'next'
+    , previous: 'previous'
+    , current: 1
   };
   for (prop in options) { 
     if (prop in options)
       this.options[prop] = options[prop];
   };
+  this.setNav();
 };
 
+Movie.prototype = {
+  showSlide: function(cell){
+    for(i=0;i<this.stages.length;i++){
+      var stage = this.stages[i]
+        , c = stage.className;
+      if(cell != i){
+        stage.className = c.replace(' current','');
+      }else{
+        c = c.replace(' current','');
+        stage.className = c + ' current';
+      }
+    };
+  }
+, next: function(){
+    if(this.options.current < this.stages.length)
+      this.showSlide(this.options.current++); 
+  }
+, previous: function(){
+    if(this.options.current > 0)
+      this.showSlide(this.options.current--); 
+  }
+, setNav: function(){
+    var nextButtons = document.getElementsByClassName(this.options.next);
+    var prevButtons = document.getElementsByClassName(this.options.previous);
+    this.each(nextButtons,function(button,i){
+      button.addEventListener('click', this.next, false);
+      if (button.captureEvents) button.captureEvents(Event.CLICK);
+    }.bind(this));
+  }
+, each: function(collection,cb){
+    for(i=0;i<collection.length;i++){
+      cb(collection[i],i);
+    };
+  }
+, event: function(e,cb){
+    var event = e || window.event;
+    event.cancelBubble = true;
+    event.stopPropagation();
+    event.stopImmediatePropagation();
+    event.preventDefault();
+    cb();
+  }
+};
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,15 @@
  */
 var Resume = require('../data/resume.json');
 exports.index = function(req, res){
+  var experience = Resume.about.experience
+  experience.forEach(function(exp,index){
+    console.log(index, ':', experience.length);
+    exp.siblings = {};
+    if(index < experience.length - 1)
+      exp.siblings.next = experience[ ( index + 1 ) ].camelName;
+    if(index != 0)
+      exp.siblings.previous = experience[ ( index - 1 ) ].camelName;
+  });
   res.render('index', { 
       title: 'Express'
     , resume: JSON.stringify(Resume) 
